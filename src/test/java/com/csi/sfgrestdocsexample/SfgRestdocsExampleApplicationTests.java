be4ecb9aca5770d265d55ec7554c89579a8c2163
prajul-shinde <prajulshinde@gmail.com>
2021-04-15 18:16:57 +0530

    initial project setup. closes #1

---
+++ src/test/java/com/csi/sfgrestdocsexample/SfgRestdocsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csi.sfgrestdocsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRestdocsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
