85ffacb260e861d24523d0b9cdf27e4be43dba6e
prajul-shinde <prajulshinde@gmail.com>
2021-04-16 14:12:03 +0530

    documenting requests. closes #8

--- src/test/java/com/csi/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/com/csi/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -25,8 +25,7 @@ import static org.mockito.BDDMockito.given;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @ExtendWith(RestDocumentationExtension.class)
@@ -74,7 +73,18 @@ class BeerControllerTest {
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerToJson))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+        .andDo(document("v1/beer",requestFields(
+                fieldWithPath("id").ignored(),
+                fieldWithPath("version").ignored(),
+                fieldWithPath("createdDate").ignored(),
+                fieldWithPath("lastModifiedDate").ignored(),
+                fieldWithPath("beerName").description("Beer Name"),
+                fieldWithPath("beerStyle").description("Beer style"),
+                fieldWithPath("upc").description("Beer upc").attributes(),
+                fieldWithPath("price").description("price of beer"),
+                fieldWithPath("quantityOnHand").ignored()
+        )));
 
     }
 
